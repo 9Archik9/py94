5ea7a8b3bd89d30ce16dbe985281f174757cbea7
9Archik9 <igro085@gmail.com>
2023-11-25 19:10:05 +0300

    add login method of authorization

--- Project/User_identification/app.py
+++ Project/User_identification/app.py
@@ -1,30 +1,38 @@
+import json
 from getters import get_email, get_password
 from validators import check_data
 
 
 def app(users):
-    while True:
-        email = get_email()
-        if email.lower() == 'exit':
-            print('Goodbye!')
-            break
+    user_choose = input('Select authorization option (write number): \n'
+                        '1. Sign up\n'
+                        '2. Login\n>')
+    if user_choose == '2':
+        while True:
+            email = get_email()
+            if email.lower() == 'exit':
+                print('Goodbye!')
+                break
 
-        password = get_password()
-        if password.lower() == 'exit':
-            print('Goodbye!')
-            break
+            password = get_password()
+            if password.lower() == 'exit':
+                print('Goodbye!')
+                break
 
-        result = check_data(email, password, users)
-        if result != 'Login error':
-            print(f'Welcome, {result}!')
-            return result
-        else:
-            print(result)
+            result = check_data(email, password, users)
+            if result != 'Login error':
+                print(f'Welcome, {result}!')
+                return result
+            else:
+                print(result)
 
 
-users = [
-        {'email': 'some@mail.ru', 'password': '123pass'},
-        {'email': 'py94@gmail.com', 'password': 'py94'}
-    ]
+with open('user_data.json', 'r') as json_file:
+    users = json.load(json_file)
+
+# users = [
+#         {'email': 'some@mail.ru', 'password': '123pass'},
+#         {'email': 'py94@gmail.com', 'password': 'py94'}
+#     ]
 
 app(users)
--- Project/User_identification/getters.py
+++ Project/User_identification/getters.py
@@ -6,3 +6,8 @@ def get_email():
 def get_password():
     password = input('Write your password: ')
     return password
+
+
+# def out_authorization(email, password):
+#     if get_email().lower() == 'exit' or get_password().lower() == 'exit':
+#         print('Goodbye!')
