e4e013eacf6b355df2adf79230ce1787b3e68763
9Archik9 <igro085@gmail.com>
2023-11-30 16:22:06 +0300

    complete homework(without '*')

--- Homework/Lesson_12/Homework/app.py
+++ Homework/Lesson_12/Homework/app.py
@@ -2,10 +2,37 @@ from base_classes import *
 
 
 class Duck(Animal):
-    ...
+    def voice(self):
+        print('Krya!')
+
+    def move(self):
+        print('Duck swimming...')
 
 
 class Tiger(Animal):
-    ...
+    def voice(self):
+        print('Hrrrr!')
+
+    def move(self):
+        print('Tiger running...')
 
 
+class Car(Transport):
+    def move(self):
+        print('Car drives')
+
+    def launch(self):
+        print('Engine is on')
+
+
+duck = Duck()
+duck.move()
+duck.voice()
+
+tiger = Tiger()
+tiger.move()
+tiger.voice()
+
+car = Car()
+car.launch()
+car.move()
