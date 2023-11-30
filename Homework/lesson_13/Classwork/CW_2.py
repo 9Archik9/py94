edb10a702bf266407516471d1d20d864fabe12ea
9Archik9 <igro085@gmail.com>
2023-11-30 21:54:55 +0300

    fix calculate discount_price

--- Homework/lesson_13/Classwork/CW_2.py
+++ Homework/lesson_13/Classwork/CW_2.py
@@ -8,17 +8,16 @@ class Price:
 
 class DiscountPrice(Price):
     def __init__(self, number, discount):
-        self.number = number
         super().__init__(number)
         self.discount = discount
 
     def get_price(self):
         original_price = super().get_price()
-        return original_price * self.discount
+        return original_price - (original_price * self.discount) / 100
 
 
 price_obj = Price(100)
 print("Price:", price_obj.get_price())
 
-discount_price_obl = DiscountPrice(100, 0.2)
+discount_price_obl = DiscountPrice(100, 20)
 print('Discount price:', discount_price_obl.get_price())
